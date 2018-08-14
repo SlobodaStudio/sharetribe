6fc3895aeec424cb40eab94d6cbd1e3688815ae5
Boyan Tabakov <boyan@sharetribe.com>
2018-08-14 15:17:41 +0300

    wip

---
+++ script/ci-install-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'deb http://deb.debian.org/debian jessie-backports main' | sudo tee -a /etc/apt/sources.list
+
+sudo apt-get update && sudo apt-get install sphinxsearch
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install 7.8
